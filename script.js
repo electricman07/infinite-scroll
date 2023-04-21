9c3e31496e11764b9a170e6977c976eafb3b813a
electricman07 <63125433+electricman07@users.noreply.github.com>
2023-04-21 11:22:41 -0700

    added getPhotos function and added API key

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
 
+let photosArray = [];
+
+// Unsplash API
+const count = 10;
+const apiKey = "-Z5OwcJvlYx7YN6Sjz7lgUPAK9qv-qO716HZ4lQfuWw";
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+
+    console.log(photosArray);
+  } catch (error) {
+    //  Catch error here
+    console.log("there was an error", error);
+  }
+}
+// on load
+getPhotos();
